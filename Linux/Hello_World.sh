992556120de48c6845a39cbbe9d365489487fbcc
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-09-21 02:48:33 +1000

    Update Hello_World.sh

--- Linux/Hello_World.sh
+++ Linux/Hello_World.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#Displaying the content of the variabel using an alternative syntax
+#You can used the curly brackets to expand the brackets.
+echo "I am learning the technique of ${WORD}ing"
 
-#This script displays various information to the screen.
-#Display the text "hello"
-echo "Hello"
+#Showing how not to append text to a variable.
+#this doe not work
+echo "$WORDing is fun" #This variabe does not exist.
+
+#Creating an new varible.
+ENDING='ed'
+
+#Combing variables
+#You can combine variable together.
+echo "This is ${WORD}${ENDING}" 
